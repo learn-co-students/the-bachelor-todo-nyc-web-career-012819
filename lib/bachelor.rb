8392e60fd0a25e00f2d5f959429efdb08290ff7e
camille feghali <camfeg@gmail.com>
2019-01-18 20:40:08 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,42 @@
+
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
   # code here
+  winner = ""
+  data[season].each do |x|
+    x.each do |info, value|
+      if value == "Winner"
+        winner = x["name"]
+      end
+    end
+  end
+  winner.split[0]
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  singer = ""
+  data.each do |season, contestants|
+    contestants.each do |x|
+      x.each do |info, value|
+        if value == occupation
+          singer = x["name"]
+      end
+      end
+    end
+  end
+  singer
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  new_hash = {}
+  data.each do |season, contestants|
+    contestants.each do |info, value|
+      binding.pry
+    end
+  end
 end
 
 def get_occupation(data, hometown)
