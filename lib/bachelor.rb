4ce787cbe90db75d9d3ee2df9d5e285456ee55cf
camille feghali <camfeg@gmail.com>
2019-01-18 21:37:56 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -31,16 +31,22 @@ end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
-  new_hash = {}
+  count = 0
   data.each do |season, contestants|
-    contestants.each do |info, value|
-      binding.pry
+    contestants.each do |x|
+      x.each do |info, value|
+        if value == hometown
+          count += 1
+        end
+      end
     end
   end
+  count
 end
 
 def get_occupation(data, hometown)
   # code here
+  data.e
 end
 
 def get_average_age_for_season(data, season)
