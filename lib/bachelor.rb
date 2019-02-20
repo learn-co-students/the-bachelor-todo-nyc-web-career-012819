82b1dd0ef8b60f6e082697e51de6597ceafefe51
akramhelil <akramhelil@gmail.com>
2019-02-19 22:15:56 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,56 +1,74 @@
-
-def get_first_name_of_season_winner(data, season)
-  data[season].each do |contestant_hash|
-    if contestant_hash["status"].downcase == "winner"
-      return contestant_hash["name"].split(" ").first
+require "pry"
+def get_first_name_of_season_winner(data,season)
+  data[season].each do |contestant_info|
+    if contestant_info["status"].downcase == "winner"
+      return contestant_info["name"].split(" ").first
     end
   end
 end
 
 
 def get_contestant_name(data, occupation)
-  # code here
   data.each do |season, contestants|
-    contestants.each do |contestant_hash|
-      if contestant_hash["occupation"]== occupation
-        return contestant_hash["name"]
+    contestants.each do |contestant_info|
+      if contestant_info["occupation"] == occupation
+        return contestant_info["name"]
       end
     end
   end
 end
+
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
-  count = 0
+  amount = 0
   data.each do |season, contestants|
-    contestants.each do |contestant_hash|
-      if contestant_hash["hometown"] == hometown
-        count += 1
+    contestants.each do |contestant_info|
+      if contestant_info["hometown"] == hometown
+        # binding.pry
+       amount += 1 #(return amount+=1 and amount +=1 diffrences)
       end
     end
   end
-  count
-  end
-
+  amount #returning the FULL amount after all the loops are ended
+end
 
 def get_occupation(data, hometown)
-  # code here
   data.each do |season, contestants|
-    contestants.each do |contestant_hash|
-    if contestant_hash["hometown"] == hometown
-      return contestant_hash["occupation"]
-        end
+    contestants.each do |contestant_info|
+      if contestant_info["hometown"] == hometown
+        return contestant_info["occupation"]
       end
     end
   end
+end
+
 
 def get_average_age_for_season(data, season)
-  # code here
   total_age = 0
-  numberOfContestants = 0
-  data[season].each do |contestant_hash|
-    total_age += (contestant_hash["age"]).to_i
-    numberOfContestants += 1
+  contestant_count = 0
+  data[season].each do |contestant_info|
+    total_age += (contestant_info['age']).to_i
+    contestant_count += 1
   end
-  (total_age/numberOfContestants.to_f).round(0)
+  (total_age/contestant_count.to_f).round
 end
+
+
+
+
+#
+
+#
+#
+#
+#
+# def get_average_age_for_season(data, season)
+#   # code here
+#   total_age = 0
+#   numberOfContestants = 0
+#   data[season].each do |contestant_hash|
+#     total_age += (contestant_hash["age"]).to_i
+#     numberOfContestants += 1
+#   end
+#   (total_age/numberOfContestants.to_f).round(0)
+# end
